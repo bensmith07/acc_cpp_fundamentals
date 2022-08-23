842f71dea4a9fafa6e5a84228d8a1cc579861b0d
Ben Smith <ben.f.smith07@gmail.com>
2022-08-23 14:07:37 -0500

    ...

---
+++ ch02_codelab_exercises.cpp
@@ -0,0 +1,53 @@
+// 10501. Write a statement that prints "Hello, world" to the screen
+cout << "Hello, world";
+
+// 10505. Given an integer variable [count], write a statement that
+//        writes the value of [count] to standard output
+cout << count;
+
+// 10733. Write the include directive needed to allow use of the various
+//        I/O operators such as cout and cin
+#include <iostream>
+
+// 10901. Write a statement that declares an int vaiable named count
+int count;
+
+// 10538. Given an integer variable drivingAge that has already beed decalred, 
+//        write a statement that assigns the value 17 to drivingAge
+drivingAge = 17;
+
+// 10902. Write a declaration of a variable named numberOfWidgets that can be 
+//        be used to hold numbers like 57 and 981 and -4
+int numberOfWidgets;
+
+// 10982. Declare a floating point variable named distance
+float distance;
+
+// 11095. Write the necessary preprocessor directive to enable the use of the C++
+//        string class
+#include <string>
+
+// 10592. Declare a variable isACustomer, suitable for representing a true or
+//        false value
+bool isACustomer;
+
+// 10511. Declare a variable temperature and initialize it to 98.6
+auto temperature = 98.6;
+
+// 10951. Write a statement to add the vlaues of x and y together, storing the 
+//        result in sum.
+sum = x + y;
+
+// 10953. Write a statement to set the value of area to the value of length times
+//        times the value of width.
+area = length * width;
+
+// 10956. Write a statement to assign kilos the value of pounds divided by 2.2
+kilos = pounds / 2.2;
+
+// 10952. Write a statement to subtract tax from gross_pay and assign the 
+//        result to net_pay
+net_pay = gross_pay - tax;
+
+// 10957. Write a statement to find the remainder rem when num is divided by 5
+rem = num % 5;
