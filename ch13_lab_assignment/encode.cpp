2853dc7e2bd72d130d3e8a3837987a9919241cb0
Ben Smith <ben.f.smith07@gmail.com>
2022-12-01 09:50:32 -0600

    ...

---
+++ ch13_lab_assignment/encode.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    char ch;
+    fstream file;
+    vector<char> file_contents;
+
+    file.open("guidelines.txt", ios::in);
+    file.get(ch);
+    file_contents.push_back(ch);
+    
+    while (file)
+    {
+        file.get(ch);
+        ch = char(int(ch) + 10);
+        file_contents.push_back(ch);
+    }
+    file.close();
+
+    file.open("guidelines.txt", ios::out);
+    
+    for(const char& ch : file_contents ) file << ch;
+
+}
